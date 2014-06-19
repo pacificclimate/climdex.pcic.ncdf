a7f3f7d0ed34dd69816907bc994bd940c7bfc6b0
David Bronaugh <bronaugh@uvic.ca>
2014-06-19 11:23:29 -0700

    Improve documentation; improve tests; move back from parallel to snow.

--- tests/test_basic_file_funcs.R
+++ tests/test_basic_file_funcs.R
@@ -10,6 +10,154 @@ correct.data.dir <- "correct_output/"
 correct.thresh.file.6190 <- paste(correct.data.dir, "thresholds_monClim_CanESM2_historical_r1i1p1_1961-1990.nc", sep="")
 thresh.omit.list <- c("tx10p", "tn10p", "tx10p", "tx90p", "wsdi", "csdi")
 
+test.get.thresholds.chunk <- function() {
+  ## Define mappings and filenames.
+  thresholds.name.map <- c(tx10thresh="tx10thresh", tn10thresh="tn10thresh", tx90thresh="tx90thresh",
+                           tn90thresh="tn90thresh", r95thresh="r95thresh", r99thresh="r99thresh")
+  thresh.files <- "correct_output/thresholds_monClim_CanESM2_historical_r1i1p1_1961-1990.nc"
+
+  if(all(file.exists(thresh.files))) {
+    ## Open files, etc.
+    cdx.funcs <- get.climdex.functions(get.climdex.variable.list("tmax"))
+    thresholds.netcdf <- lapply(thresh.files, nc_open)
+    t.f.idx <- get.var.file.idx(thresholds.name.map, lapply(thresholds.netcdf,
+                                                            ncdf4.helpers::nc.get.variable.list, min.dims=2))
+    
+    ## Get thresholds chunk.
+    dat <- get.thresholds.chunk(list(), cdx.funcs, thresholds.netcdf, t.f.idx, thresholds.name.map)
+    checkEquals(thresholds.chunk.tmax.only, dat)
+    
+    lapply(thresholds.netcdf, nc_close)
+  }
+}
+
+test.compute.indices.for.stripe <- function() {
+  ## Define mappings and filenames.
+  author.data <- list(institution="Looney Bin", institution_id="LBC")
+  input.files <- list.files("test1/", full.names=TRUE)
+  variable.name.map <- c(tmax="tasmax", tmin="tasmin", prec="pr")
+  thresh.files <- "correct_output/thresholds_monClim_CanESM2_historical_r1i1p1_1961-1990.nc"
+
+  if(length(input.files) > 0 && all(file.exists(input.files)) && all(file.exists(thresh.files))) {
+    ## Open files, etc.
+    cdx.funcs <- get.climdex.functions(get.climdex.variable.list("prec"))
+    f <- lapply(input.files, ncdf4::nc_open)
+    f.meta <- create.file.metadata(f, variable.name.map)
+    climdex.var.list <- get.climdex.variable.list(names(f.meta$v.f.idx), "all", NULL)
+    cdx.meta <- get.climdex.variable.metadata(climdex.var.list, input.files[1])
+
+    thresholds.name.map <- c(tx10thresh="tx10thresh", tn10thresh="tn10thresh", tx90thresh="tx90thresh",
+                             tn90thresh="tn90thresh", r95thresh="r95thresh", r99thresh="r99thresh")
+    thresholds.netcdf <- lapply(thresh.files, nc_open)
+    t.f.idx <- get.var.file.idx(thresholds.name.map, lapply(thresholds.netcdf,
+                                                            ncdf4.helpers::nc.get.variable.list, min.dims=2))
+    thresh.dat <- get.thresholds.chunk(list(), cdx.funcs, thresholds.netcdf, t.f.idx, thresholds.name.map)
+    
+    ## Compute indices for stripe
+    cdx <- compute.indices.for.stripe(list(X=1:2, Y=1:2), cdx.funcs, f.meta$ts, c(1981, 1990), f.meta$dim.axes,
+                                      f.meta$v.f.idx, variable.name.map, f.meta$src.units, f.meta$dest.units,
+                                      t.f.idx, thresholds.name.map, f=f, thresholds.netcdf=thresholds.netcdf)
+
+    lapply(thresholds.netcdf, nc_close)
+
+    res <- lapply(names(cdx[[1]]), function(x) {
+      fn <- list.files("correct_output/", pattern=paste("^", x, sep=""), full.names=TRUE)
+      f.valid <- nc_open(fn, readunlim=FALSE)
+      d.input <- ncvar_get(f.valid, strsplit(x, "_")[[1]][1])
+      nc_close(f.valid)
+      d.comparison <- t(do.call(cbind, lapply(cdx, function(cr) { cr[[x]] })))
+      dim(d.comparison) <- dim(d.input)
+      
+      ## Apparently there are differences at the 3e-6 level between calculated and saved data... who knew?
+      checkEquals(d.input, d.comparison, tolerance=1e-5)
+      mean(abs(d.input - d.comparison))
+    })
+    
+    lapply(f, nc_close)
+  }
+  invisible(0)
+}
+
+test.get.quantiles.for.stripe <- function() {
+  historical.files <- list.files("historical/", full.names=TRUE)
+  if(length(historical.files) > 0) {
+    ## FIXME: This is untestable with the current input data.
+    
+    ## Establish basic inputs.
+    author.data <- list(institution="Looney Bin", institution_id="LBC")
+    input.files <- list.files("test1/", full.names=TRUE)
+    
+    ## Prepare derived inputs.
+    f <- lapply(input.files, ncdf4::nc_open)
+    variable.name.map <- c(tmax="tasmax", tmin="tasmin", prec="pr")
+    f.meta <- create.file.metadata(f, variable.name.map)
+    threshold.dat <- get.thresholds.metadata(names(f.meta$v.f.idx))
+    
+    ## Compute threshold quantiles for stripe
+    q <- get.quantiles.for.stripe(list(Y=1), f.meta$ts, c(1981, 1990), f.meta$dim.axes,
+                                  f.meta$v.f.idx, variable.name.map, f.meta$src.units,
+                                  f.meta$dest.units, f)
+    
+    ## FIXME: Compare to valid data.
+    
+    lapply(f, nc_close)
+  }
+}
+
+test.get.quantiles.object <- function() {
+  ## Define mappings and filenames.
+  thresholds.name.map <- c(tx10thresh="tx10thresh", tn10thresh="tn10thresh", tx90thresh="tx90thresh",
+                           tn90thresh="tn90thresh", r95thresh="r95thresh", r99thresh="r99thresh")
+  thresh.files <- "correct_output/thresholds_monClim_CanESM2_historical_r1i1p1_1961-1990.nc"
+
+  if(all(file.exists(thresh.files))) {
+    ## Open files, etc.
+    cdx.funcs <- get.climdex.functions(get.climdex.variable.list("tmax"))
+    thresholds.netcdf <- lapply(thresh.files, nc_open)
+    t.f.idx <- get.var.file.idx(thresholds.name.map, lapply(thresholds.netcdf,
+                                                            ncdf4.helpers::nc.get.variable.list, min.dims=2))
+    ## Get thresholds chunk.
+    dat <- get.thresholds.chunk(list(Y=1), cdx.funcs, thresholds.netcdf, t.f.idx, thresholds.name.map)
+    
+    ## Get quantiles object for index 2
+    q <- get.quantiles.object(dat, 2)
+    
+    ## FIXME: Compare to a correct object.
+    
+    lapply(thresholds.netcdf, nc_close)
+  }
+}
+
+test.get.northern.hemisphere.booleans <- function() {
+  test.get.nh <- function(test.dir) {
+    input.files <- list.files(test.dir, full.names=TRUE)
+    f <- lapply(input.files, ncdf4::nc_open)
+    f.v <- lapply(f, ncdf4.helpers::nc.get.variable.list, min.dims=2)
+    bools <- get.northern.hemisphere.booleans(list(X=1:2, Y=1:2), f[[1]], f.v[[1]], NULL)
+    lapply(f, ncdf4::nc_close)
+    return(bools)
+  }
+  ## FIXME: Need test data.
+  browser()
+  if(file.exists("test3/"))
+    checkEquals(test.get.nh("test3/"), rep(FALSE, 4))
+  ## FIXME: Need test data, and a projected dataset.
+  ##checkEquals(test.get.nh("test7/"), correct.data)
+}
+
+## FIXME: Needs proper test data. This is just a framework...
+test.get.data <- function() {
+  test.dir <- "test3/"
+  variable.name.map <- c(tmax="tasmax", tmin="tasmin", prec="pr")
+  if(file.exists(test.dir)) {
+    input.files <- list.files(test.dir, full.names=TRUE)
+    f <- lapply(input.files, ncdf4::nc_open)
+    f.meta <- create.file.metadata(f, variable.name.map)
+    d <- get.data(f[[f.meta$v.f.idx['prec']]], "pr", list(Y=2), "kg m-2 s-1", "kg m-2 s-1", c(lon="X",lat="Y",time="T"))
+    lapply(f, ncdf4::nc_close)
+  }    
+}
+
 ## FIXME: Needs proper test data. This is just a framework...
 test.file.funcs <- function() {
   test.dir <- "test3/"
--- tests/test_var_meta.R
+++ tests/test_var_meta.R
@@ -26,6 +26,25 @@ test.get.climdex.functions <- function() {
   checkEquals(climdex.functions$tmax.tmin.prec.all.notfclimdex, get.climdex.functions(climdex.var.list$tmax.tmin.prec.all, FALSE))
 }
 
-test.create.cmip5.climdex.filenames <- function() {
+test.create.climdex.cmip5.filenames <- function() {
+  fn.split <- c(model="CanESM2", emissions="rcp45", run="r1i1p1", tstart="20100101", tend="20991231")
+
+  valid.tmax.mon.fn <- c("txxETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc", "txnETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc",
+                         "tx10pETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc", "tx90pETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc")
+  valid.tmax.all.fn <- c("suETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc", "idETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc",
+                         "txxETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc", "txxETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc",
+                         "txnETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc", "txnETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc",
+                         "tx10pETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc", "tx10pETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc",
+                         "tx90pETCCDI_mon_CanESM2_rcp45_r1i1p1_201001-209912.nc", "tx90pETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc",
+                         "wsdiETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc", "altwsdiETCCDI_yr_CanESM2_rcp45_r1i1p1_2010-2099.nc")
   
+  checkEquals(valid.tmax.mon.fn, create.climdex.cmip5.filenames(fn.split, climdex.var.list$tmax.mon))
+  checkEquals(valid.tmax.all.fn, create.climdex.cmip5.filenames(fn.split, climdex.var.list$tmax.all))
 }
+
+test.flatten.dims <- function() {
+  dat <- structure(1:8, .Dim=c(2, 2, 2))
+  valid.flat <- structure(1:8, .Dim = c(2L, 4L))
+  checkEquals(flatten.dims(dat, 2:3), valid.flat)
+}
+
